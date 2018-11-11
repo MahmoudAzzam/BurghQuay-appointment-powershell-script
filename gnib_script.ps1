c2602b833d03f5c430115479820325d30af686be
MahmoudAzzam <mahmoud.azzam22@gmail.com>
2018-11-11 23:49:22 +0000

    Add files via upload

---
+++ gnib_script.ps1
@@ -0,0 +1,12 @@
+$Url = "Get the URL of the appointmnet type required"
+$j=Invoke-WebRequest -Uri $Url
+$jParsed = $j | ConvertFrom-Json 
+$EmailFrom = "Sender email" 
+$EmailTo = "Receiver email" 
+$Body = "https://burghquayregistrationoffice.inis.gov.ie/Website/AMSREG/AMSRegWeb.nsf/AppSelect?OpenForm"  
+$SMTPServer = "smtp.gmail.com"  
+$SMTPClient = New-Object Net.Mail.SmtpClient($SmtpServer, 587)  
+$SMTPClient.EnableSsl = $true  
+$SMTPClient.Credentials = New-Object System.Net.NetworkCredential("SEmail's username without @domain", "Email's password")
+while($jParsed.empty -eq 'TRUE'){$jParsed=Invoke-WebRequest -Uri $Url | ConvertFrom-Json
+if($jParsed.empty -ne 'TRUE'){$SMTPClient.Send($EmailFrom, $EmailTo, "Available appointment", ($jParsed | ConvertTo-Json))}}$SMTPClient.Send($EmailFrom, $EmailTo, "Error", ($jParsed | ConvertTo-Json))
